142a7e4ec924edba0482077888158bb2d1601aef
Luis Dueñas <luis.alonso.duenas.m@gmail.com>
2018-09-27 13:59:38 -0700

    refactor

--- Controllers/ConsultasController.js
+++ Controllers/ConsultasController.js
@@ -1,36 +1,60 @@
 var express = require('express');
 var router = express.Router();
+let consultaResult;
+let consultaCampoResult;
+let consultaid;
+let nombreConsulta;
+
 
 router.route('/consultas').post(function (req, res) {
     if (!req.body) {
         res.json({ "Message": "tiraste mal el post, baboso" });
     }
     else {
-        let select = req.body.select.split("\n");
-        let consultaid = req.body.consultaid;
-        let nombreConsulta = req.body.nombreConsulta;
-        let result = [];
-        let prioridad = 0;
-        select.forEach(element => {
-            if (!element) {
-                return;
-            }
-            prioridad++;
-            let lastIndex = element.toLowerCase().lastIndexOf(" as ");
-            let campo = element.substring(0, lastIndex);
-            let nombreCampo = element.substring(lastIndex + 4, element.length - 1);
-            result.push(
-                "INSERT INTO Ganadera.dbo.ConsultaCampo VALUES ('" + consultaid + "', '" + nombreConsulta + "','"
-                + campo.trim()
-                + "','" + nombreCampo.replace(',', '').trim() + "','" + nombreCampo.replace(',', '').trim() + "','100','2','',''," + prioridad + ",GETDATE(),GETDATE())"
-            );
-        });
-        res.json({ "MESSAGE": result });
+
+        if (!req.body.select) {
+            res.json({ "Message": "te falto meter el select animal" });
+        }
+        else if(!req.body.from){
+            res.json({ "Message": "te falto meter el from estupido" });
+        }
+        else if (!req.body.consultaid){
+            res.json({ "Message": "y el ID de la consulta cara de pene?" });
+        }
+        else if(!req.body.nombreConsulta){
+            res.json({ "Message": "crees que soy adivino o que?, pon el pinche nombre de la consulta" });
+        }
+        else {
+            consultaid = req.body.consultaid;
+            nombreConsulta = req.body.nombreConsulta;
+            generaConsultaCampo(req.body.select);
+        }
     }
+    res.json({ "Consulta":consultaResult,"ConsultaCampo": consultaCampoResult });
 });
-router.use(function timeLog (req, res, next) {
+router.use(function timeLog(req, res, next) {
     console.log('Time: ', Date.now())
     next()
 })
+
+function generaConsultaCampo(_select) {
+    consultaCampoResult = [];
+    let select = _select.split("\n");
+    let prioridad = 0;
+    select.forEach(element => {
+        if (!element) {
+            return;
+        }
+        prioridad++;
+        let lastIndex = element.toLowerCase().lastIndexOf(" as ");
+        let campo = element.substring(0, lastIndex);
+        let nombreCampo = element.substring(lastIndex + 4, element.length - 1);
+        consultaCampoResult.push(
+            "INSERT INTO Ganadera.dbo.ConsultaCampo VALUES ('" + consultaid + "', '" + nombreConsulta + "','"
+            + campo.trim()
+            + "','" + nombreCampo.replace(',', '').trim() + "','" + nombreCampo.replace(',', '').trim() + "','100','2','',''," + prioridad + ",GETDATE(),GETDATE())"
+        );
+    });
+}
 
 module.exports = router;
