c0ab67602862be59709e994b9d582d57da70b438
alecsisduarte <alexis.duarte@hotmail.com>
2018-10-01 23:06:10 -0700

    Se agregó una carpeta con utilidades para realizar tareas sencillas y globales,  se promovio la utilización de inicializadores tales como let y const y se removió toda utilización de variables globales

--- Controllers/ConsultasController.js
+++ Controllers/ConsultasController.js
@@ -1,75 +1,97 @@
-var express = require('express');
-var router = express.Router();
-let consultaResult;
-let consultaCampoResult;
-let consultaid;
-let nombreConsulta;
+const express = require('express'),
+    router = express.Router()
+    BodyParser = require('../Utils/BodyParser');
 
 
-router.route('/consultas').post(function (req, res) {
+router.route('/consultas').post((req, res) => {
     if (!req.body) {
-        res.json({ "Message": "tiraste mal el post, baboso" });
+        res.json({
+            "Message": "tiraste mal el post, baboso"
+        });
     }
-    else {
 
-        if (!req.body.select) {
-            res.json({ "Message": "te falto meter el select animal" });
-        }
-        else if (!req.body.from) {
-            res.json({ "Message": "te falto meter el from estupido" });
-        }
-        else if (!req.body.consultaid) {
-            res.json({ "Message": "y el ID de la consulta cara de pene?" });
-        }
-        else if (!req.body.nombreConsulta) {
-            res.json({ "Message": "crees que soy adivino o que?, pon el pinche nombre de la consulta" });
-        }
-        else {
-            consultaid = req.body.consultaid;
-            nombreConsulta = req.body.nombreConsulta;
-            generaConsultaCampo(req.body.select);
-            generateConsulta(req.body.from, req.body.where, req.body.orderby, req.body.groupby);
-        }
+    let body = BodyParser.toLowerCaseKeys(req.body);
+    if (!body.select) {
+        res.status(400).send({
+            "Message": "te falto meter el select animal"
+        });
+        return;
+    } else if (!body.from) {
+        res.status(400).send({
+            "Message": "te falto meter el from estupido"
+        });
+        return;
+    } else if (!body.consultaid) {
+        res.status(400).send({
+            "Message": "y el ID de la consulta cara de pene?"
+        });
+        return;
+    } else if (!body.nombreconsulta) {
+        res.status(400).send({
+            "Message": "crees que soy adivino o que?, pon el pinche nombre de la consulta"
+        });
+        return;
     }
-    res.json({ "Delete": "DELETE FROM ConsultaCampo WHERE ConsultaId = " + consultaid + ";DELETE FROM Consulta WHERE ConsultaId = " + consultaid + ";", "Consulta": consultaResult, "ConsultaCampo": consultaCampoResult });
-});
-router.use(function timeLog(req, res, next) {
-    console.log('Time: ', Date.now())
-    next()
-})
-router.route('/consultas').get(function(req,res){
-    res.json({ "LOL": " te awitaste alv" });
-});
 
-function generaConsultaCampo(_select) {
-    consultaCampoResult = [];
-    let select = _select.split(/\n|\r/gm);
-    let prioridad = 0;
-    select.forEach(element => {
-        if (!element) {
-            return;
-        }
-        prioridad++;
-        let lastIndex = element.toLowerCase().lastIndexOf(" as ");
-        let campo = element.substring(0, lastIndex);
-        let nombreCampo = element.substring(lastIndex + 4, element.length - 1);
-        consultaCampoResult.push(
-            "INSERT INTO Ganadera.dbo.ConsultaCampo VALUES ('" + consultaid + "', '" + nombreConsulta + "','"
-            + campo.trim()
-            + "','" + nombreCampo.replace(',', '').trim() + "','" + nombreCampo.replace(',', '').trim() + "','100','2','',''," + prioridad + ",GETDATE(),GETDATE())"
-        );
+    //Limpiamos y arreglamos el cuerpo de la petición
+    body = BodyParser.makeDoubleQuoted(BodyParser.cleanBody(body));
+    
+    const consultaid = body.consultaid;
+    const nombreconsulta = body.nombreconsulta;
+
+    const consultaCampo = generaConsultaCampo(body.select, consultaid, nombreconsulta);
+    const consulta = generateConsulta(body.from, body.where, body.orderby, body.groupby, nombreconsulta);
+
+    res.status(200).send({
+        "Delete": `DELETE FROM ConsultaCampo WHERE ConsultaId = ${consultaid}; DELETE FROM Consulta WHERE ConsultaId = ${consultaid};`,
+        "Consulta": consulta,
+        "ConsultaCampo": consultaCampo
     });
+});
+
+router.use(function timeLog(req, res, next) {
+    console.log('Time: ', Date.now());
+    next();
+});
+
+router.route('/consultas').get((req, res) => {
+    res.json({
+        "LOL": " te awitaste alv"
+    });
+});
+
+/**
+ * Genera los consultas campos apartir de la porción SELECT del script
+ * @param {String} _select Porción del SELECT del Script a dividir
+ * @returns {Array<String>} Array de String que contienen los inserts de consultaCampo
+ */
+function generaConsultaCampo(_select, consultaid, nombreConsulta) {
+    const regSelect = /([\w\.]+)\ (?:AS\ |\[)(\w+)\]{0,1}\ *(?:,(?:\ |\n|\ |\r|\t)*)*/gi;
+    let execSelect;
+    let index = 0;
+    let consultaCampoResult = ['INSERT INTO Ganadera.dbo.ConsultaCampo VALUES '];
+    while ((execSelect = regSelect.exec(_select))) {
+        const campo = execSelect[1];
+        const nombreCampo = execSelect[2];
+        const insert = createConsultaCampoInsert(consultaid, nombreConsulta, campo, nombreCampo, index++);
+        consultaCampoResult.push(insert);
+    }
+    //Removemos la ultima coma
+    consultaCampoResult[index - 1] = consultaCampoResult[index - 1].substring(0, consultaCampoResult[index - 1].length - 1);
+    return consultaCampoResult;
 }
 
-function generateConsulta(_from, _where, _orderby, _groupby) {
-    let regex = /(\t|\n|\r)+|\ {2,}/gm;
-    let from = _from.replace(regex, "");
-    let where = _where.replace(regex, "");
-    let orderby = _orderby.replace(regex, "");
-    let groupby = _groupby.replace(regex, "");
-    consultaResult =
-        "INSERT INTO Ganadera.dbo.Consulta VALUES ('100', '" + nombreConsulta
-        + "', '" + from.trim() + "', '" + where.trim() + "','" + orderby.trim() + "','" + groupby.trim() + "','','1','','','','',GETDATE(),GETDATE())";
+function generateConsulta(from, where, orderby, groupby, nombreConsulta) {
+    where = where || '';
+    orderby = orderby || "";
+    groupby = groupby || "";
+    return `INSERT INTO Ganadera.dbo.Consulta VALUES ('100', '${nombreConsulta}', '${from.trim()}', '${where.trim()}','${orderby.trim()}','${groupby.trim()}','','1','','','','',GETDATE(),GETDATE())`;
+}
+
+
+function createConsultaCampoInsert(consultaid, nombreConsulta, campo, nombreCampo, prioridad) {
+    const nomCam = nombreCampo.replace(',', '').trim();
+    return `('${consultaid}', '${nombreConsulta}', '${campo.trim()}', '${nomCam}', '${nomCam}', '100', '2', '', '', ${prioridad}, GETDATE(), GETDATE())`;
 }
 
 module.exports = router;
